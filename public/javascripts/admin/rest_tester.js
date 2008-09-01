6ea840a627c6ad8fc6995bc01dc2fcc29e009502
Luca Greco <luca.greco@alcacoop.it>
2008-09-01 19:40:59 +0200

    added license notices

--- public/javascripts/admin/rest_tester.js
+++ public/javascripts/admin/rest_tester.js
@@ -1,12 +1,22 @@
-/*
- * REST Tester 0.1
- * Copyright(c) 2008, Alca Società Cooperativa
- * info@alca.le.it - http://alca.le.it
- *
- * licensed GPL3
- *
- * based of ExtJS (http://extjs.com/license)
- */
+/****************************************************************************
+ITILSheets - ITIL-based ticketing webapplication
+Copyright (C) 2008 - Alca Società Cooperativa
+
+    http://alca.le.it - info@alca.le.it
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
 
 const DEBUG = 0;
 
